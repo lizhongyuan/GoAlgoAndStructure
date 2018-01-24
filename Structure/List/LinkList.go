d6ab276f66fc96ffb46b75927dcd83f758d7c4ef
lizhongyuan <hybridblader@163.com>
2018-01-25 02:05:57 +0800

    init

---
+++ Structure/List/LinkList.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type Node struct {
+  data int
+  next *Node
+}
+
+func main() {
+  var node1 Node
+  var node2 Node
+  node1.data = 1
+  node2.data = 2
+  node1.next = &node2
+
+  fmt.Printf("node1's data:%d\n", node1.data);
+  fmt.Printf("node2's data:%d\n", node1.next.data);
+}
