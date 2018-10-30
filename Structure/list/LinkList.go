5fff144f004eef1f8dfad0fa500a2f6fb5186398
lizhongyuan <fenixlee@163.com>
2018-10-30 14:56:27 +0800

    调整了结构, 增加了测试文件

--- Structure/list/LinkList.go
+++ Structure/list/LinkList.go
@@ -1,4 +1,5 @@
-package main
+package list
+
 
 import "fmt"
 
@@ -7,13 +8,13 @@ type Node struct {
   next *Node
 }
 
-func main() {
+func ShowList() {
   var node1 Node
   var node2 Node
   node1.data = 1
   node2.data = 2
   node1.next = &node2
 
-  fmt.Printf("node1's data:%d\n", node1.data);
-  fmt.Printf("node2's data:%d\n", node1.next.data);
+  fmt.Printf("node1's data:%d\n", node1.data)
+  fmt.Printf("node2's data:%d\n", node1.next.data)
 }
