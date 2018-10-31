7b74a594a1a30b6f7e89dc564e879c5be67995d4
lizhongyuan <fenixlee@163.com>
2018-10-31 14:32:22 +0800

    tmp

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import "fmt"
-import "./Structure"
+
+/*
+type Item struct {
+	dataType uintptr
+	data uint64
+}
+
+item1 := &Item{&"uint64", 1}
+*/
+
+type test1 struct {
+	a uint64
+	b uint64
+}
+
+var testA *test1 = &test1{1, 2}
+
 
 func main() {
 
 	fmt.Println("main func")
-
-	arr := [1,2,3]
 
 	Structure.ShowList()
 	Structure.ShowBubbleSort(&arr)
